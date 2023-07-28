65fc16746ea5b565527c6e0f21fd4214607dda85
oleh.trunov <oleh.trunov@gmail.com>
2023-07-28 16:01:10 +0200

    update app main page

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import {  NextPage } from 'next';
+import WeatherApp from '@/components/WeatherApp';
+
+const HomePage: NextPage = () => {
+
+  return (
+
+     <WeatherApp/>
+  );
+};
+
+export default HomePage;
